c8089f8748fd52c41f718926e9015164f0476ff4
Map7e <70562018+qaz492888660@users.noreply.github.com>
2026-09-21 05:13:57 +0800

    Add Capacitor configuration

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.map7e.reader',
+  appName: 'MAP7E Reader',
+  webDir: 'dist'
+};
+
+export default config;
